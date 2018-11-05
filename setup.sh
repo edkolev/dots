fa4e631654f40f8f20f087d0216896618117ded7
Evgeni Kolev <evgenysw@gmail.com>
2018-11-05 17:43:52 +0200

    Docker detachKeys

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-for link in bashrc inputrc vimrc gitconfig git_template tmux.conf hammerspoon; do
-  [[ -e ~/.${link} ]] && echo "Skipping $link" && continue
-  ln -s ${BASEDIR}/${link} ~/.${link} && echo "Linked $link"
-done
+function link {
+    local a="$1"
+    local b="$2"
+
+    [[ -e "$b" ]] && echo "Skipping $b" && return
+    ln -s "$a" "$b" && echo "Linked $a => $b"
+}
+
+link "bashrc"        ~/.bashrc
+link "inputrc"       ~/.inputrc
+link "vimrc"         ~/.vimrc
+link "gitconfig"     ~/.gitconfig
+link "git_template"  ~/.git_template
+link "tmux.conf"     ~/.tmux.conf
+link "hammerspoon"   ~/.hammerspoon
+mkdir -p ~/.docker && link "docker_config" ~/.docker/config.json
