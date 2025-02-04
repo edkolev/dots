7b8b4f5383d394a4dfa64108f839e19ff646dc5b
Evgeni Kolev <evgenysw@gmail.com>
2025-02-04 23:18:25 +0200

    Hammerspoon: improvement to Cmd-L/H

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -20,22 +20,27 @@ local function equalUnitRect(f1, f2)
    return f1.x == f2.x and f1.y == f2.y and string.format("%.1f", f1.w) == string.format("%.1f", f2.w)
 end
 
-local function moveWindowToUnit(win, r1, r2)
-   local currentR = hs.geometry.toUnitRect(win:frame(), win:screen():frame())
-
-   if equalUnitRect(r1, currentR) then
-      win:moveToUnit(r2)
-   else
-      win:moveToUnit(r1)
+local function nextUnitRect(r, rs)
+   for i,v in pairs(rs) do
+      if equalUnitRect(v, r) and i ~= #rs then
+         return rs[i+1]
+      end
    end
+   return rs[1]
+end
+
+local function moveWindowToUnit(win, rs)
+   local currentR = hs.geometry.toUnitRect(win:frame(), win:screen():frame())
+   local r = nextUnitRect(currentR, rs)
+   win:moveToUnit(r)
 end
 
 hs.hotkey.bind({"cmd", "shift"}, "H", function()
-  moveWindowToUnit(hs.window.focusedWindow(), hs.layout.left50, hs.layout.left70)
+      moveWindowToUnit(hs.window.focusedWindow(), {hs.layout.left50, hs.layout.left70, hs.layout.left30})
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "L", function()
-  moveWindowToUnit(hs.window.focusedWindow(), hs.layout.right50, hs.layout.right70)
+      moveWindowToUnit(hs.window.focusedWindow(), {hs.layout.right50, hs.layout.right70, hs.layout.right30})
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "f", function()
