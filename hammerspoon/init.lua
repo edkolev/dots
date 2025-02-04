4a479db05471b02624b957dcd05b1479a764599c
Evgeni Kolev <evgenysw@gmail.com>
2025-02-04 22:25:44 +0200

    Hammerspoon: enhance left/right win resize with 70%
    Both 50% and 70% resize are now supported.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,30 +16,26 @@ for key, app in pairs(apps) do
    end)
 end
 
-hs.hotkey.bind({"cmd", "shift"}, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+local function equalUnitRect(f1, f2)
+   return f1.x == f2.x and f1.y == f2.y and string.format("%.1f", f1.w) == string.format("%.1f", f2.w)
+end
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+local function moveWindowToUnit(win, r1, r2)
+   local currentR = hs.geometry.toUnitRect(win:frame(), win:screen():frame())
+
+   if equalUnitRect(r1, currentR) then
+      win:moveToUnit(r2)
+   else
+      win:moveToUnit(r1)
+   end
+end
+
+hs.hotkey.bind({"cmd", "shift"}, "H", function()
+  moveWindowToUnit(hs.window.focusedWindow(), hs.layout.left50, hs.layout.left70)
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+  moveWindowToUnit(hs.window.focusedWindow(), hs.layout.right50, hs.layout.right70)
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "f", function()
